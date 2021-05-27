1a2f24c3650ab365298c54815bb3f7890f9459e7
Alfahmist <48145002+alfahmist@users.noreply.github.com>
2021-05-27 09:12:09 +0700

    Jwt
    + Services\JwtService
    + Middleware\AuthencticationExtention

---
+++ API/Services/JwtService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public class JwtService
+    {
+        private readonly string _secret;
+        private readonly string _expDate;
+
+        public JwtService(IConfiguration config)
+        {
+            _secret = config.GetSection("JwtConfig").GetSection("secret").Value;
+            _expDate = config.GetSection("JwtConfig").GetSection("expirationInMinutes").Value;
+        }
+
+        public string GenerateSecurityToken(string email, string name)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    //tambahkan data yang dibutuhkan
+                    new Claim(ClaimTypes.Email, email),
+                    new Claim(ClaimTypes.Name, name)
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(double.Parse(_expDate)),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+
+        }
+    }
+}
