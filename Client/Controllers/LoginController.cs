a944c2fe3e406f4e3a3ddfd5ebde144e7b101158
Alfahmi <gst.alfahmi@gmail.com>
2021-06-01 18:03:52 +0700

    Fix appsettingjs

--- Client/Controllers/LoginController.cs
+++ Client/Controllers/LoginController.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API.ViewModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Client.Controllers
@@ -23,6 +28,15 @@ namespace Client.Controllers
             HttpContext.Session.SetString(SessionName, loginEmail);
             HttpContext.Session.SetInt32(SessionAge, 24);
             return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public HttpStatusCode Login(LoginVM loginVM)
+        {
+            var httpclient = new HttpClient();
+            StringContent stringContent = new StringContent(JsonConvert.SerializeObject(loginVM), Encoding.UTF8, "application/json");
+            var result = httpclient.PostAsync("https://localhost:44387/api/Account/Login/", stringContent).Result;
+            return result.StatusCode;
         }
     }
 }
