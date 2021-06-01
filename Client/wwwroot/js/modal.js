c9e4bbe308449e5e66e3612f59fee6018dd18958
Alfahmi <gst.alfahmi@gmail.com>
2021-06-01 13:21:09 +0700

    Create modal.js

---
+++ Client/wwwroot/js/modal.js
@@ -0,0 +1,27 @@
+﻿var ModalFlow = state => {
+    switch (state) {
+        case "forgot":
+            $('#exampleModalLongTitle').text("Forgot Password");
+            $('#buttonModal').text("Send Email");
+            $('#registerName').hide();
+            $('#registerPassword').hide();
+            $('#registerPhone').hide();
+            $('#registerGender').hide();
+            $('#registerPhone').hide();
+            $('#registerDate').hide();
+            break;
+        case "register":
+            $('#exampleModalLongTitle').text("Register");
+            $('#buttonModal').text("Register");
+            $('#registerName').show();
+            $('#registerPassword').show();
+            $('#registerPhone').show();
+            $('#registerGender').show();
+            $('#registerPhone').show();
+            $('#registerDate').show();
+            break;
+      
+        default:
+        // code block
+    }
+}
