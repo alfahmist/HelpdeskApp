d32c135830925e1361ca60486b140595b2e872f1
Paulus Siahaan <siahaan.paulus123@gmail.com>
2021-06-04 10:08:06 +0700

    updated_getNewTicketStatus

---
+++ API/ViewModel/ResetVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.ViewModel
+{
+    public class ResetVM
+    {
+        public string Password { get; set; }
+        public string Token { get; set; }
+    }
+}
