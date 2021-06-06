a7fca4085287406cff554cd6b3ef2f970033c2b0
Paulus Siahaan <siahaan.paulus123@gmail.com>
2021-06-06 23:01:56 +0700

    Add_TicketDetailView

--- Client/Controllers/DashboardController.cs
+++ Client/Controllers/DashboardController.cs
@@ -33,6 +33,10 @@ namespace Client.Controllers
         {
             this.myContext = myContext;
         }
+        readonly HttpClient client = new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:44397/API/")
+        };
         [HttpGet]
         public async Task<List<InprogressTicketVM>> AllNewTicketStatus()
         {
@@ -48,6 +52,99 @@ namespace Client.Controllers
             ticketNumber = progressTicket.Count;
 
             return progressTicket;
+        }
+
+        [Route("TicketDetail/{id}")]
+        public IActionResult TicketDetail(string id)
+        {
+            var token = HttpContext.Session.GetString("JWToken");
+            if (token != null)
+            {
+                var jwtReader = new JwtSecurityTokenHandler();
+                var jwt = jwtReader.ReadJwtToken(token);
+
+                var email = jwt.Claims.First(e => e.Type == "email").Value;
+                var emailDb = myContext.Employees.FirstOrDefault(emp => emp.Email == email);
+                var empId = emailDb.Id;
+                if (id == "")
+                {
+                    return RedirectToAction("Index", "Dashboard");
+                }
+
+                ViewData["ticketID"] = id;
+                ViewData["empId"] = empId;
+                var tCount = GetTicketMessage(id);
+                return View("TicketDetail");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+        }
+        [HttpPost]
+        public HttpStatusCode NewTicketMessage(SendMessageVM model)
+        {
+            var httpClient = new HttpClient();
+            StringContent content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+            var result = httpClient.PostAsync("https://localhost:44397/api/Tickets/NewTicketMessage", content).Result;
+            return result.StatusCode;
+        }
+
+        public JsonResult TicketDetailById(string ticketId)
+        {
+
+            //var id = Request.Query["ticketId"];
+            var responseTask = client.GetAsync($"Tickets/TicketDetailById/{ticketId}");
+            //responseTask.Wait();
+            var result = responseTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsStringAsync();
+                readTask.Wait();
+                var tickets = readTask.Result;
+                return Json(tickets);
+            }
+            return Json(null);
+        }
+        [Route("Dashboard/Account")]
+        public IActionResult Account()
+        {
+            var token = HttpContext.Session.GetString("JWToken");
+            if (token != null)
+            {
+                var jwtReader = new JwtSecurityTokenHandler();
+                var jwt = jwtReader.ReadJwtToken(token);
+
+
+                var name = jwt.Claims.First(c => c.Type == "unique_name").Value;
+                var email = jwt.Claims.First(e => e.Type == "email").Value;
+                var emailDb = myContext.Employees.FirstOrDefault(emp => emp.Email == email);
+                var empId = emailDb.Id;
+
+                ViewData["name"] = name;
+                ViewData["empId"] = empId;
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            
+        }
+
+        public async Task<IList<TicketMessageVM>> GetTicketMessage(string id)
+        {
+
+            var responseTask = client.GetAsync($"Tickets/GetTicketMessage/{id}");
+            //responseTask.Wait();
+            var result = responseTask.Result;
+
+            string apiResponse = await result.Content.ReadAsStringAsync();
+            var ticketMessage = JsonConvert.DeserializeObject<List<TicketMessageVM>>(apiResponse);
+            ViewData["tCount"] = ticketMessage.Count();
+            return ticketMessage;
+
         }
         public IActionResult Index()
         {
@@ -168,24 +265,24 @@ namespace Client.Controllers
             return closedTickets;
         }
         
-        [HttpGet]
-        public async Task<List<InprogressTicketVM>> AllNewTicketStatus()
-        {
-            progressTicket = new List<InprogressTicketVM>();
-            using (var httpClient = new HttpClient(clientHandler))
-            {
-                using (var response = await httpClient.GetAsync("https://localhost:44397/api/Tickets/GetAllTicketUpdates/"))
-                {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    progressTicket = JsonConvert.DeserializeObject<List<InprogressTicketVM>>(apiResponse);
+        //[HttpGet]
+        //public async Task<List<InprogressTicketVM>> AllNewTicketStatus()
+        //{
+        //    progressTicket = new List<InprogressTicketVM>();
+        //    using (var httpClient = new HttpClient(clientHandler))
+        //    {
+        //        using (var response = await httpClient.GetAsync("https://localhost:44397/api/Tickets/GetAllTicketUpdates/"))
+        //        {
+        //            string apiResponse = await response.Content.ReadAsStringAsync();
+        //            progressTicket = JsonConvert.DeserializeObject<List<InprogressTicketVM>>(apiResponse);
                   
             
-                }
-            }  
-            TicketAllCount = progressTicket.Count;
+        //        }
+        //    }  
+        //    TicketAllCount = progressTicket.Count;
           
-            return progressTicket;
-        }
+        //    return progressTicket;
+        //}
 
         [HttpGet]
         public async Task<List<InprogressTicketVM>> GetInprogressTicket()
