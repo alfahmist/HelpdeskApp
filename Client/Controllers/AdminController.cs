f9f18d1987cd2e5a44391d502a1c5932da773b7a
Alfahmi <gst.alfahmi@gmail.com>
2021-06-08 08:23:35 +0700

    Update AdminController.cs

--- Client/Controllers/AdminController.cs
+++ Client/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using API.Models;
 using API.ViewModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
@@ -14,15 +15,48 @@ namespace Client.Controllers
 {
     public class AdminController : Controller
     {
-
+        const string SessionName = "session";
         readonly HttpClient client = new HttpClient
         {
             BaseAddress = new Uri("https://localhost:44397/API/")
         };
+
+
         // Index
+        [Route("admin")]
         public IActionResult Index()
         {
-            return View();
+            var session = HttpContext.Session.GetString(SessionName);
+            if (session != null)
+            {
+                return View();
+            }
+            else
+            {
+                return View("Login");
+            }
+            
+        }
+
+        [Route("admin/login")]
+        public IActionResult Login()
+        {
+            var session = HttpContext.Session.GetString(SessionName);
+            if (session != null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [Route("admin/logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove(SessionName);
+            return RedirectToAction("Login");
         }
         public async Task<List<Employee>> Employee()
         {
@@ -39,13 +73,14 @@ namespace Client.Controllers
 
             return employees;
         }
-
-        //Register
+        
+        [Route("admin/register")]
         public IActionResult Register()
         {
             return View("register");
         }
-        [HttpPost("admin/ajaxregister")]
+
+        [HttpPost("ajaxregister")]
         public HttpStatusCode AjaxRegister(RegisterVM registerVM)
         {
             var httpClient = new HttpClient();
@@ -53,10 +88,21 @@ namespace Client.Controllers
             var result = httpClient.PostAsync("https://localhost:44397/api/Accounts/Register", content).Result;
             return result.StatusCode;
         }
-        public IActionResult Login()
+
+       
+        [HttpPost("ajaxlogin")]
+        public HttpStatusCode AjaxLogin(LoginVM loginVM)
         {
-            return View("login");
+            if(loginVM.Email == "admin@admin.com" && loginVM.Password == "admin")
+            {
+                HttpContext.Session.SetString(SessionName, "Admin");
+                return HttpStatusCode.OK;
+            } else
+            {
+                return HttpStatusCode.Unauthorized;
+            }
         }
 
+       
     }
 }
