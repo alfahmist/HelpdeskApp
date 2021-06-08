5cc6ddd6a70c85d1bdbc02ab173db1daf51824a2
Paulus Siahaan <siahaan.paulus123@gmail.com>
2021-06-08 21:38:34 +0700

    get Employee

--- API/Controllers/EmployeeController.cs
+++ API/Controllers/EmployeeController.cs
@@ -7,6 +7,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Repositories.Data;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace API.Controllers
 {
@@ -15,9 +19,30 @@ namespace API.Controllers
     public class EmployeeController : BaseController<Employee, EmployeeRepository, string>
     {
         private readonly EmployeeRepository employeeRepository;
-        public EmployeeController(EmployeeRepository employeeRepository) : base(employeeRepository)
+        private IConfiguration Configuration;
+        public EmployeeController(EmployeeRepository employeeRepository, IConfiguration Configuration) : base(employeeRepository)
         {
             this.employeeRepository = employeeRepository;
+            this.Configuration = Configuration;
+        }
+        
+
+        [HttpGet("GetEmployee")]
+        public IEnumerable<dynamic> GetEmployee()
+        {
+
+            var dbparams = new DynamicParameters();
+            using IDbConnection db = new SqlConnection(Configuration.GetConnectionString("MyConnection"));
+            return db.Query<dynamic>("[dbo].[SP_GetEmployee]", dbparams, commandType: CommandType.StoredProcedure);
+           
+        }
+        [HttpGet("GetOpenTickets")]
+        public IEnumerable<dynamic> GetOpenTickets()
+        {
+            var dbparams = new DynamicParameters();
+            dbparams.Add("statusId", 1, DbType.String);
+            using IDbConnection db = new SqlConnection(Configuration.GetConnectionString("MyConnection"));
+            return db.Query<dynamic>("[dbo].[SP_GetStatusByID]", dbparams, commandType: CommandType.StoredProcedure);
         }
     }
 }
