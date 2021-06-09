cab7959b7267ce8934b0eb60b5db967846331a8c
Alfahmi <gst.alfahmi@gmail.com>
2021-06-09 16:34:51 +0700

    Create AssignEmployeeVM.cs

---
+++ API/ViewModel/AssignEmployeeVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.ViewModel
+{
+    public class AssignEmployeeVM
+    {
+        public string EmpId{ get; set; }
+        public string TicketId{ get; set; }
+    }
+}
