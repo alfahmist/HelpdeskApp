6742dab522350f328d1ef538d0d3bc2f18009bee
Alfahmi <gst.alfahmi@gmail.com>
2021-06-09 15:06:36 +0700

    Create AssignHistoryVM.cs

---
+++ Client/ViewModels/AssignHistoryVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.ViewModels
+{
+    public class AssignHistoryVM
+    {
+        public string Name { get; set; }
+        public DateTime AssignDate { get; set; }
+    }
+}
